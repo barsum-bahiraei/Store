e8a63910ad0971a469d55ec2a561f659874ae73c
barsum <barsum.bahiraei@gmail.com>
2025-07-07 01:27:03 +0330

    add entity login

---
+++ Backend/Src/Core/Store.Domain/Users/UserEntity.cs
@@ -0,0 +1,9 @@
+namespace Store.Domain.Users;
+
+public class UserEntity : BaseEntity
+{
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
