772e56f805894a8d0dfc23bbef3d7f8fe8df6a7f
barsum <barsum.bahiraei@gmail.com>
2025-12-23 21:25:29 +0330

    .

--- Backend/Src/Infrastructure/Store.Persistent/Implementation/CategoryRepository.cs
+++ Backend/Src/Infrastructure/Store.Persistent/Implementation/CategoryRepository.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Store.Persistent.Database.Sql;
 
 namespace Store.Persistent.Implementation;
 
 public class CategoryRepository(AppDbContext context) : ICategoryRepository
 {
-    public Task<List<CategoryEntity>> ListAsync(CancellationToken cancellation)
+    public async Task<List<CategoryEntity>> ListAsync(CancellationToken cancellation)
     {
-        throw new NotImplementedException();
+        var result = await context.Categories.ToListAsync(cancellation);
+        return result;
     }
 
     public async Task<CategoryEntity> CreateAsync(CategoryEntity input, CancellationToken cancellation)
