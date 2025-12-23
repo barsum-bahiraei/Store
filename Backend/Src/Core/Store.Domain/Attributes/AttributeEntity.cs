260174d89fab10863c093807323a781dc3a742e7
barsum <barsum.bahiraei@gmail.com>
2025-12-23 20:37:43 +0330

    .

--- Backend/Src/Core/Store.Domain/Attributes/AttributeEntity.cs
+++ Backend/Src/Core/Store.Domain/Attributes/AttributeEntity.cs
@@ -5,14 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Store.Shared.Enums;
 
 namespace Store.Domain.Attributes;
 
 public class AttributeEntity : BaseEntity
 {
     public string Name { get; set; }
-    public string Type { get; set; }
-    public string Unit { get; set; }
+    public AttributeTypeEnum Type { get; set; }
+    public AttributeUnitEnum Unit { get; set; }
     public List<CategoryAttributeEntity> CategoryAttributes { get; set; }
     public List<ProductAttributeEntity> ProductAttributes { get; set; }
 }
