331dad3cfb6aeec64334c0bbfd8f292dcd9ca78c
barsum <barsum.bahiraei@gmail.com>
2025-12-23 20:35:15 +0330

    .

--- Backend/Src/Core/Store.Service/Products/ProductService.cs
+++ Backend/Src/Core/Store.Service/Products/ProductService.cs
@@ -11,43 +11,27 @@ namespace Store.Service.Products;
 
 public class ProductService(IProductRepository productRepository) : IProductService
 {
-    public async Task<ProductCreateOutput> CreateAsync(ProductCreateInput input, CancellationToken cancellation)
-    {
-        var productEntity = new ProductEntity
-        {
-            Name = input.Name,
-            Description = input.Description,
-            Price = input.Price,
-        };
-        var product = await productRepository.CreateAsync(productEntity, cancellation);
-        var result = new ProductCreateOutput
-        {
-            Name = product.Name,
-            Price = product.Price,
-            DiscountPrice = product.DiscountPrice,
-            Id = product.Id,
-            Description = product.Description,
-        };
-        return result;
-    }
-
-    public Task DeleteAsync(CancellationToken cancellation)
-    {
-        throw new NotImplementedException();
-    }
-
     public Task DetailAsync(int id, CancellationToken cancellation)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<List<ProductListOutput>> ListAsync(ProductListInput input, CancellationToken cancellation)
+    public Task<List<ProductListOutput>> ListAsync(ProductListInput input, CancellationToken cancellation)
     {
-        var products = await productRepository.ListAsync(input, cancellation);
-        return products;
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductCreateOutput> CreateAsync(ProductCreateInput input, CancellationToken cancellation)
+    {
+        throw new NotImplementedException();
     }
 
     public Task UpdateAsync(CancellationToken cancellation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(CancellationToken cancellation)
     {
         throw new NotImplementedException();
     }
